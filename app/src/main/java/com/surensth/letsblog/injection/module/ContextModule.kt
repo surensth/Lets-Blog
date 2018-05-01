579ae9dbb69a362e7eec428bc7cba73f97427b89
Surendra Shrestha <developer.suren@gmail.com>
2018-05-01 13:31:31 +0545

    create ContextModule to inject context

---
+++ app/src/main/java/com/surensth/letsblog/injection/module/ContextModule.kt
@@ -0,0 +1,24 @@
+package com.surensth.letsblog.injection.module
+
+import android.app.Application
+import android.content.Context
+import com.surensth.letsblog.base.BaseView
+import dagger.Provides
+
+/**
+ * Created by surensth on 5/1/18.
+ */
+object ContextModule {
+    @Provides
+    @JvmStatic
+    internal fun provideContext(baseView: BaseView): Context {
+        return baseView.getContext()
+    }
+
+    @Provides
+    @JvmStatic
+    internal fun provideApplication(context: Context): Application {
+        return context.applicationContext as Application
+    }
+
+}
