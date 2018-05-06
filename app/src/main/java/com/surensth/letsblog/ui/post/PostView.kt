ffdc2c6d75f6a4f21d8343e47b20f0ab8f30e637
Surendra Shrestha <developer.suren@gmail.com>
2018-05-06 11:44:49 +0545

    add postView

---
+++ app/src/main/java/com/surensth/letsblog/ui/post/PostView.kt
@@ -0,0 +1,38 @@
+package com.surensth.letsblog.ui.post
+
+import android.support.annotation.StringRes
+import com.surensth.letsblog.base.BaseView
+import com.surensth.letsblog.model.Fixtures
+
+/**
+ * Created by surensth on 5/6/18.
+ */
+interface PostView : BaseView {
+    /**
+     * updates the fixtures to the view
+     */
+    fun updateFixtures(posts: List<Fixtures>)
+
+    /**
+     * shows error string
+     */
+    fun showError(error: String)
+
+    /**
+     * shows error
+     * @param errorResId the resource id of error to display in view
+     */
+    fun showError(@StringRes errorResId: Int) {
+        this.showError(getContext().getString(errorResId))
+    }
+
+    /**
+     * display loading in view
+     */
+    fun showLoading()
+
+    /**
+     * hide loading from view
+     */
+    fun hideLoading()
+}
