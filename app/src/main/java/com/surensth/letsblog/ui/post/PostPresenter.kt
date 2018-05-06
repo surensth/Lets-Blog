f208ca9ddbc7018121e0247621acd1915da23a31
Surendra Shrestha <developer.suren@gmail.com>
2018-05-06 11:49:24 +0545

    create  postPresenter

---
+++ app/src/main/java/com/surensth/letsblog/ui/post/PostPresenter.kt
@@ -0,0 +1,14 @@
+package com.surensth.letsblog.ui.post
+
+import com.surensth.letsblog.base.BasePresenter
+
+/**
+ * Created by surensth on 5/6/18.
+ */
+class PostPresenter(postView: PostView) : BasePresenter<PostView>(postView) {
+    override fun onViewCreated() {
+    }
+
+    override fun onViewDestroyed() {
+    }
+}
