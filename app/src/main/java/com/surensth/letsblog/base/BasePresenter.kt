00df277f44943775ce8e73f51496a6c8916f2819
Surendra Shrestha <developer.suren@gmail.com>
2018-05-06 15:46:59 +0545

    add dependency injection in inject method of BasePresenter

--- app/src/main/java/com/surensth/letsblog/base/BasePresenter.kt
+++ app/src/main/java/com/surensth/letsblog/base/BasePresenter.kt
@@ -1,9 +1,21 @@
 package com.surensth.letsblog.base
 
+import com.surensth.letsblog.injection.component.DaggerPresenterInjector
+import com.surensth.letsblog.injection.component.PresenterInjector
+import com.surensth.letsblog.injection.module.ContextModule
+import com.surensth.letsblog.injection.module.NetworkModule
+import com.surensth.letsblog.ui.post.PostPresenter
+
 /**
  * Created by surensth on 4/29/18.
  */
 abstract class BasePresenter<out V : BaseView>(protected val view: V) {
+    private val injector: PresenterInjector = DaggerPresenterInjector
+            .builder()
+            .baseView(view)
+            .contextModule(ContextModule)
+            .networkModule(NetworkModule)
+            .build()
     init {
         inject()
     }
@@ -26,6 +38,8 @@ abstract class BasePresenter<out V : BaseView>(protected val view: V) {
      * injects required dependencies
      */
     private fun inject() {
-
+        when (this) {
+            is PostPresenter -> injector.inject(this)
+        }
     }
 }
