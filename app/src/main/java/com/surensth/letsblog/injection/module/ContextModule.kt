72cce745f87a1f2e3ef24eb3288e23fb20ed9ee0
Surendra Shrestha <developer.suren@gmail.com>
2018-05-06 15:43:59 +0545

    fix build issue

--- app/src/main/java/com/surensth/letsblog/injection/module/ContextModule.kt
+++ app/src/main/java/com/surensth/letsblog/injection/module/ContextModule.kt
@@ -3,17 +3,20 @@ package com.surensth.letsblog.injection.module
 import android.app.Application
 import android.content.Context
 import com.surensth.letsblog.base.BaseView
+import dagger.Module
 import dagger.Provides
 
 /**
  * Created by surensth on 5/1/18.
  */
+@Module
 object ContextModule {
     @Provides
     @JvmStatic
     internal fun provideContext(baseView: BaseView): Context {
         return baseView.getContext()
     }
+
 
     @Provides
     @JvmStatic
